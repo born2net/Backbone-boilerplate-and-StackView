8c016f26ffc6c71772a8b19fea0bc91cff63e34c
Sean <slevy@mediasignage.com>
2014-04-21 08:49:28 -0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,51 @@
+/**
+ StudioLite MediaSignage Inc (c) open source digital signage project.
+ Visit Github for license and docs: http://git.digitalsignage.com
+ @class StudioLite
+ @constructor
+ @return {Object} instantiated StudioLite
+ **/
+define(['underscore', 'jquery', 'backbone', 'bootstrap', 'backbone.controller', 'ComBroker', 'Lib', 'Jalapeno', 'JalapenoHelper'], function (_, $, Backbone, Bootstrap, backbonecontroller, ComBroker, Lib, Jalapeno, JalapenoHelper) {
+    var StudioLite = Backbone.Controller.extend({
+
+        // app init
+        initialize: function () {
+            window.BB = Backbone;
+            BB.globs = {};
+            BB.SERVICES = {};
+            BB.EVENTS = {};
+            BB.LOADING = {};
+            BB.CONSTS = {};
+            BB.globs['UNIQUE_COUNTER'] = 0;
+            BB.globs['RC4KEY'] = '226a3a42f34ddd778ed2c3ba56644315';
+            BB.lib = new Lib();
+            BB.lib.addBackboneViewOptions();
+            BB.comBroker = new ComBroker();
+            BB.comBroker.name = 'AppBroker';
+            BB.Jalapeno = new Jalapeno();
+            _.extend(BB.Jalapeno,BB.comBroker);
+            BB.Jalapeno.clearServices();
+            BB.Jalapeno.name = 'JalapenoBroker';
+            BB.JalapenoHelper = new JalapenoHelper();
+            window.jalapeno = BB.Jalapeno;
+            window.log = BB.lib.log;
+
+            // internationalization
+            require(['localizer'], function () {
+                var lang = "en";
+                var opts = { language: lang, pathPrefix: "./_lang" };
+                $("[data-localize]").localize("local", opts);
+            });
+
+            // router init
+            require(['LayoutRouter'], function (LayoutRouter) {
+                var LayoutRouter = new LayoutRouter();
+                BB.history.start();
+                BB.comBroker.setService(BB.SERVICES['LAYOUT_ROUTER'], LayoutRouter);
+                LayoutRouter.navigate('authenticate/_/_', {trigger: true});
+            })
+        }
+    });
+
+    return StudioLite;
+});
---
+++ init.js
@@ -0,0 +1,48 @@
+/**
+ Require js initialization module definition file for StudioLite
+ @class Require init js
+ **/
+require.config({
+    waitSeconds: 25,
+    baseUrl: '/_mscontent/',
+    paths: {
+        'jquery': '_common/_jquery/std/jq1.9.1/jquery-1.9.1',
+        'backbone': '_common/_js/backbone/backbone',
+        'backbone.controller': '_common/_js/backbone-controller/backbone.controller',
+        'ComBroker': '_controllers/ComBroker',
+        'underscore': '_common/_js/underscore/underscore',
+        'bootstrap': '_common/_js/bootstrap/js/bootstrap',
+        'Elements': 'Elements',
+        'LayoutRouter': '_controllers/LayoutRouter',
+        'StackView': '_views/StackView'
+    },
+
+    shim: {
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        'backbone.controller': {
+            deps: ['underscore', 'jquery']
+        },
+        'LayoutRouter': {
+            deps: ['Elements', 'backbone.controller']
+        },
+        'underscore': {
+            exports: '_'
+        },
+        'bootstrap': {
+            deps: ['jquery']
+        },
+        'ComBroker': {
+            deps: ['backbone', 'jquery']
+        },
+        'Elements': {
+            exports: 'Elements'
+        }
+    }
+});
+
+require(['App'], function (StudioLite) {
+    new StudioLite();
+});
